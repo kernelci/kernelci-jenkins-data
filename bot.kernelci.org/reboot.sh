cecf28248a223ec4dba563002d58cdbf1e4e4222
Guillaume Tucker <guillaume.tucker@collabora.com>
2021-11-01 10:14:51 +0000

    bot.kernelci.org/reboot.sh: delete k8s pods that aren't Running
    Iterate over all the Kubernetes clusters and delete all pods that
    aren't in the Running state.  This is to clean up any leaked pod and
    free up resources.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- bot.kernelci.org/reboot.sh
+++ bot.kernelci.org/reboot.sh
@@ -2,6 +2,32 @@
 
 set -e
 
+for ctx in \
+    "aks-kci-france-central" \
+    "aks-kci-uk-south" \
+    "gke_android-kernelci-external_europe-west1-d_kci-eu-west1" \
+    "gke_android-kernelci-external_europe-west4-c_kci-eu-west4" \
+    ; do
+    echo "[$ctx]"
+    ssh kernelci-jenkins-europe.westeurope.cloudapp.azure.com \
+        sudo su buildslave "/bin/sh -c '\
+        kubectl --context $ctx \
+        delete pod --field-selector=status.phase!=Running'"
+done
+
+for ctx in \
+    "aks-kci-us-east2" \
+    "gke_android-kernelci-external_us-central1-c_kci-us-central1" \
+    "gke_android-kernelci-external_us-east4-c_kci-big-us-east4" \
+    "gke_android-kernelci-external_us-west1-a_kci-us-west1" \
+    ; do
+    echo "[$ctx]"
+    ssh kernelci-jenkins-usa.westus2.cloudapp.azure.com \
+        sudo su buildslave "/bin/sh -c '\
+        kubectl --context $ctx \
+        delete pod --field-selector=status.phase!=Running'"
+done
+
 for host in \
     "kernelci1.eastus.cloudapp.azure.com" \
     "kernelci6.westus2.cloudapp.azure.com" \
