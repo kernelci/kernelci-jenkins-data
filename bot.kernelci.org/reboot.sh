96348b9b830c2a21d345d455d361637621f13b9b
Denys Fedoryshchenko <denys.f@collabora.com>
2023-06-12 19:57:53 +0300

    reboot.sh: Update cluster name and runner hosts
    As we migrate to new Azure account we need to update AKS and runner hostnames
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- bot.kernelci.org/reboot.sh
+++ bot.kernelci.org/reboot.sh
@@ -2,26 +2,32 @@
 
 set -e
 
+echo "Cleaning big clusters"
+
 for ctx in \
-    "aks-kci-uk-south" \
+    "aks-kbuild-big-1" \
     "gke_android-kernelci-external_europe-west1-d_kci-eu-west1" \
     "gke_android-kernelci-external_europe-west4-c_kci-eu-west4" \
     ; do
     echo "[$ctx]"
-    ssh kernelci-jenkins-usa.westus2.cloudapp.azure.com \
+    ssh kernelci-prod-jenkins-runner-3.westus3.cloudapp.azure.com \
         sudo su buildslave "/bin/sh -c '\
         kubectl --context $ctx \
         delete pod --field-selector=status.phase!=Running'"
 done
 
+echo "Cleaning medium clusters"
+
 for ctx in \
-    "aks-kci-us-east2" \
+    "aks-kbuild-medium-1" \
+    "aks-kbuild-medium-2" \
+    "aks-kbuild-medium-3" \
     "gke_android-kernelci-external_us-central1-c_kci-us-central1" \
     "gke_android-kernelci-external_us-east4-c_kci-big-us-east4" \
     "gke_android-kernelci-external_us-west1-a_kci-us-west1" \
     ; do
     echo "[$ctx]"
-    ssh kernelci-jenkins-usa.westus2.cloudapp.azure.com \
+    ssh kernelci-prod-jenkins-runner-3.westus3.cloudapp.azure.com \
         sudo su buildslave "/bin/sh -c '\
         kubectl --context $ctx \
         delete pod --field-selector=status.phase!=Running'"
