9bd6608a399027bdce3915e97e65af3bf8aebbc5
Vince Hillier <vince@revenni.com>
2023-08-21 11:07:00 -0400

    workspace cleanup and remove old runners

--- bot.kernelci.org/reboot.sh
+++ bot.kernelci.org/reboot.sh
@@ -5,7 +5,6 @@ set -e
 echo "Cleaning big clusters"
 
 for ctx in \
-    "aks-kbuild-big-1" \
     "gke_android-kernelci-external_europe-west1-d_kci-eu-west1" \
     "gke_android-kernelci-external_europe-west4-c_kci-eu-west4" \
     ; do
@@ -19,9 +18,6 @@ done
 echo "Cleaning medium clusters"
 
 for ctx in \
-    "aks-kbuild-medium-1" \
-    "aks-kbuild-medium-2" \
-    "aks-kbuild-medium-3" \
     "gke_android-kernelci-external_us-central1-c_kci-us-central1" \
     "gke_android-kernelci-external_us-east4-c_kci-big-us-east4" \
     "gke_android-kernelci-external_us-west1-a_kci-us-west1" \
@@ -33,9 +29,14 @@ for ctx in \
         delete pod --field-selector=status.phase!=Running'"
 done
 
+# Jenkins workspace cleanup
+WORKSPACE_PATH="/data/workspace/bot.kernelci.org/"
+host="kernelci-prod-jenkins-runner-3.westus3.cloudapp.azure.com"
+workspace="build-trigger"
+echo "[$host] Deleting $WORKSPACE_PATH/$workspace/workspace/*"
+ssh $host sudo "sh -c 'rm -rf $WORKSPACE_PATH/$workspace/workspace/*'"
+
 for host in \
-    "kernelci-prod-jenkins-runner-1.westus3.cloudapp.azure.com" \
-    "kernelci-prod-jenkins-runner-2.westus3.cloudapp.azure.com" \
     "kernelci-prod-jenkins-runner-3.westus3.cloudapp.azure.com" \
     ; do
     echo "[$host]"
